38a55e4a28d7e815a09176466abba6e8d9d0f39e
LeNatuo <565937294@qq.com>
2024-01-03 18:32:44 +0800

    1

---
+++ FmodStudio/Assets/Scripts/Editor/WwiseEditor.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector.Editor;
+using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityToolbarExtender;
+
+public static class WwiseEditor {
+    private static string WwisePath = Application.streamingAssetsPath + "/Audio/GeneratedSoundBanks/Windows/SoundbanksInfo.xml";
+
+    [MenuItem("CustomEditor/Wwise/CreateWwiseEvent")]
+    static void ToEventName() {
+        CreateWwiseEvent();
+    }
+
+    private static void CreateWwiseEvent() {
+    }
+}
